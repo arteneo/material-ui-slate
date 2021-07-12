c9122860033c2b4c1a714ab1dae908eb05e718a9
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2021-07-12 14:43:43 +0200

    Add proper exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import { Descendant } from "slate";
 import Slate, { SlateProps } from "@arteneo/material-ui-slate/components/Slate";
+import ElementButton, { ElementButtonProps } from "@arteneo/material-ui-slate/components/ElementButton";
+import MarkButton, { MarkButtonProps } from "@arteneo/material-ui-slate/components/MarkButton";
+import RenderElement, { RenderElementProps } from "@arteneo/material-ui-slate/components/RenderElement";
+import RenderLeaf, { RenderLeafProps } from "@arteneo/material-ui-slate/components/RenderLeaf";
+import Toolbar, { ToolbarProps } from "@arteneo/material-ui-slate/components/Toolbar";
+import DeserializeElementPropsInterface from "@arteneo/material-ui-slate/definitions/DeserializeElementPropsInterface";
+import DeserializeElementType from "@arteneo/material-ui-slate/definitions/DeserializeElementType";
+import DeserializeType from "@arteneo/material-ui-slate/definitions/DeserializeType";
+import EditorType from "@arteneo/material-ui-slate/definitions/EditorType";
+import ElementType from "@arteneo/material-ui-slate/definitions/ElementType";
+import ElementTypeType from "@arteneo/material-ui-slate/definitions/ElementTypeType";
+import FormattedTextInterface from "@arteneo/material-ui-slate/definitions/FormattedTextInterface";
+import SerializeInlineResultAttributesInterface from "@arteneo/material-ui-slate/definitions/SerializeInlineResultAttributesInterface";
+import SerializeInlineResultInteface from "@arteneo/material-ui-slate/definitions/SerializeInlineResultInteface";
+import SlatePluginInterface from "@arteneo/material-ui-slate/definitions/SlatePluginInterface";
 import SlatePluginsType from "@arteneo/material-ui-slate/definitions/SlatePluginsType";
+import TextType from "@arteneo/material-ui-slate/definitions/TextType";
 import {
     isElementActive,
     isMarkActive,
@@ -36,7 +52,28 @@ export {
     Descendant,
     Slate,
     SlateProps,
+    ElementButton,
+    ElementButtonProps,
+    MarkButton,
+    MarkButtonProps,
+    RenderElement,
+    RenderElementProps,
+    RenderLeaf,
+    RenderLeafProps,
+    Toolbar,
+    ToolbarProps,
+    DeserializeElementPropsInterface,
+    DeserializeElementType,
+    DeserializeType,
+    EditorType,
+    ElementType,
+    ElementTypeType,
+    FormattedTextInterface,
+    SerializeInlineResultAttributesInterface,
+    SerializeInlineResultInteface,
+    SlatePluginInterface,
     SlatePluginsType,
+    TextType,
     isElementActive,
     isMarkActive,
     toggleMark,
